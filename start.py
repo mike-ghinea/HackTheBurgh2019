3898233e62ee8d1efa3dd3d07d0bb3f36022c321
Mihail-Emanuel Ghinea <mihailemanuelghinea@gmail.com>
2019-03-16 18:42:07 +0000

    Change eventsFunctions to return an array of objects

--- start.py
+++ start.py
@@ -1,30 +1,7 @@
-import hmac
-import hashlib
-import urllib2
-import json
-import timestring
-import requests
-from pprint import pprint
-from eventObject import eventObject
+import eventFunctions
 
-import sys
-reload(sys)
-sys.setdefaultencoding('utf8')
+eventFunctions.getEventsByFestival("fringe")
 
-api_key    = 'V37UcPDfsVDiaYgs'
-secret_key = 'yOjBov7vc-0op26qkHmaLOL4abzb34oK'
-festival   = 'fringe'
-query      = '/events?festival=' + festival + '&year=2019&key=' + api_key
+#eventFunctions.getEventsByGenre("events")
 
-signature  = hmac.new(secret_key, query, hashlib.sha1).hexdigest()
-url        = \
-'https://api.edinburghfestivalcity.com' + query + '&signature=' + signature
 
-response = requests.get(url)
-data = json.loads(response.text)
-for x in data:
-  try:
-    pprint(eventObject(x))
-    print ""
-  except:
-    print(eventObject(x))
