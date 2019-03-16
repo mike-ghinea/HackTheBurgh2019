1f405001db193ac2492db1604c022b686a7a0505
Anna Buck <anna.buck@student.manchester.ac.uk>
2019-03-16 18:46:03 +0000

    resolving merge conflicts?

--- start.py
+++ start.py
@@ -3,16 +3,3 @@ import eventFunctions
 eventFunctions.getEventsByFestival("fringe")
 
 #eventFunctions.getEventsByGenre("events")
-
-
-<<<<<<< HEAD
-response = requests.get(url)
-data = json.loads(response.text)
-for x in data:
-  try:
-    pprint(eventObject(x))
-    print ""
-  except:
-    print(eventObject(x))
-=======
->>>>>>> 3898233e62ee8d1efa3dd3d07d0bb3f36022c321
